6da53c5a4eeab28a406415b3f6737ab4c908bc98
dtytarenko <dmutro.tutarenko@gmail.com>
2020-11-26 23:39:49 +0200

    created SearchBar component, installed axios, install connect to Unsplash API

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import axios from 'axios';
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization:
+          'Client-ID OLLJrlKU8sGAg9eoEp_vKyel5tZbGOmDuZYbuw5-OJA',
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
+
+export default App;
