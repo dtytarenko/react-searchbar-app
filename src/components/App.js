32ab66625e2ceeba18632c6e30f47788cb0f16ff
dtytarenko <dmutro.tutarenko@gmail.com>
2020-11-26 23:53:35 +0200

    setted onSearchSubmit function as async

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,16 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization:
           'Client-ID OLLJrlKU8sGAg9eoEp_vKyel5tZbGOmDuZYbuw5-OJA',
       },
     });
+
+    console.log(response);
   }
 
   render() {
